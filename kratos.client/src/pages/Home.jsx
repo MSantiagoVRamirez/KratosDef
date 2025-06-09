ca1538c7613d87bc973605134141b39fe61819b3
jextus <129642498+jextus@users.noreply.github.com>
2025-06-09 16:43:56 -0500

    Update Home.jsx

--- kratos.client/src/pages/Home.jsx
+++ kratos.client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import ActividadEconomicaService from '../Services/actividadEconomicaService';
 
 const Home = () => {
