7c4ca99e16e96ae757b1c4940d1c05a697dae3db
jextus <129642498+jextus@users.noreply.github.com>
2025-06-09 16:46:05 -0500

    Update actividadEconomicaServices.js

--- kratos.client/src/Services/actividadEconomicaServices.js
+++ kratos.client/src/Services/actividadEconomicaServices.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const API_URL = "https://localhost:7054/api/ActividadEconomica";
+const API_URL = "https://localhost:7054/api/ActividadEconomicas";
 axios.defaults.withCredentials = true;
 
-// Definición del modelo de ActividadEconomica (solo como referencia, sin tipos)
-const ActividadEconomica = {
+// DefiniciÃ³n del modelo de ActividadEconomica (solo como referencia, sin tipos)
+const ActividadEconomicas = {
     id: 0,
     codigoCiiu: 0,
     nombre: "",
@@ -18,12 +18,12 @@ const get = (id) => {
     return axios.get(`${API_URL}/consultar`, { params: { 'Id': id } });
 }
 
-const create = (ActividadEconomica) => {
-    return axios.post(`${API_URL}/insertar`, ActividadEconomica);
+const create = (ActividadEconomicas) => {
+    return axios.post(`${API_URL}/insertar`, ActividadEconomicas);
 }
 
-const update = (ActividadEconomica) => {
-    return axios.put(`${API_URL}/editar`, ActividadEconomica);
+const update = (ActividadEconomicas) => {
+    return axios.put(`${API_URL}/editar`, ActividadEconomicas);
 }
 
 const remove = (id) => {
